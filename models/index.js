1ca8e8d18f533d7a4293f9440a2b0200c1e496bf
justin-aquino <justin.aquino25@gmail.com>
2022-04-01 09:44:23 -0700

    prep for deploy

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
+require("dotenv").config
+
 const mongoose = require('mongoose')
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/katims'
 
 mongoose.connect(MONGODB_URI)
-
+// console.log(process.env.MONGODB_URI)
 const db = mongoose.connection
 
 
